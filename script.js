2c2286bbc199744a19a27aa3a09c9a088950d847
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-10-18 01:23:29 -0300

    bootstrap

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const btnSalvar = document.getElementById('btn-novo');
 const btnSalvarAlteracao = document.getElementById('btn-Alteracao');
 
+
 let idLista;
 
-btnSalvarAlteracao.classList.add('ocultar-btn');
+// btnSalvarAlteracao.classList.add('ocultar-btn');
 
 btnSalvarAlteracao.addEventListener('click', salvarAlteracao);
 btnSalvar.addEventListener('click', adicionarItem);
@@ -64,8 +65,10 @@ function alterar(id) {
         document.getElementById('quantidade').value = obj.quantidade;
         document.getElementById('preco').value = obj.preco;
 
+        btnSalvar.classList.remove('btn');
         btnSalvar.classList.add('ocultar-btn');
         btnSalvarAlteracao.classList.remove('ocultar-btn');
+        btnSalvarAlteracao.classList.add('btn');
     }
 }
 
@@ -95,7 +98,9 @@ function salvarAlteracao() {
     document.getElementById('preco').value = "";
 
     btnSalvar.classList.remove('ocultar-btn');
+    btnSalvar.classList.add('btn');
     btnSalvarAlteracao.classList.add('ocultar-btn');
+    btnSalvarAlteracao.classList.remove('btn');
     
     receita(); // Atualiza a receita aqui
 }
@@ -107,22 +112,60 @@ function mostrarLista() {
     const itens = JSON.parse(localStorage.getItem('itens')) || [];
 
     itens.forEach(item => {
+        const div = document.createElement('div');
+        div.classList.add('col-12','d-flex', 'justify-content-center', 'align-items-center', 'gap-3');
         const li = document.createElement('li');
-        li.textContent = `Produto: ${item.produto} - Quantidade: ${item.quantidade} - Preço: R$${item.preco.toFixed(2)}`;
+        const divSpan = document.createElement('div');
+        const spanId = document.createElement('span');
+        const spanProduto = document.createElement('span');
+        const spanQtde = document.createElement('span');
+        const spanPreco = document.createElement('span');
+        divSpan.classList.add('col-6','d-flex', 'justify-content-start'  ,'align-content-center', 'gap-1');
+        
+        spanId.textContent = `${item.id}`;
+        spanId.classList.add('col-1');
+        spanProduto.textContent = `${item.produto}`;
+        spanProduto.classList.add('col-4', 'text-break');
+        spanQtde.textContent = `QTDE: ${item.quantidade}`;
+        spanQtde.classList.add('col-3');
+        spanPreco.textContent = `R$ ${item.preco.toFixed(2)}`;
+        spanPreco.classList.add('col-5', 'text-break');
+
+        divSpan.appendChild(spanId);
+        divSpan.appendChild(spanProduto);
+        divSpan.appendChild(spanQtde);
+        divSpan.appendChild(spanPreco);
+        
+        div.appendChild(divSpan);
+        
         
         const btnAlterar = document.createElement('button');
+        btnAlterar.classList.add('btn', 'btn-warning', 'btn-sm');
         const btnRemover = document.createElement('button');
+        btnRemover.classList.add('btn', 'btn-danger', 'btn-sm');
         btnAlterar.innerText = 'Alterar';
         btnRemover.innerText = 'Remover';
 
         btnRemover.addEventListener('click', () => { remover(item.id) });
         btnAlterar.addEventListener('click', () => { alterar(item.id) });
         
-        const div = document.createElement('div');
-        div.appendChild(btnAlterar);
-        div.appendChild(btnRemover);
+        const divBtn = document.createElement('div');
+        divBtn.classList.add('col-5', 'd-flex', 'justify-content-center', 'gap-2');
+        
+        divBtn.appendChild(btnAlterar);
+        divBtn.appendChild(btnRemover);
+
+        div.appendChild(divBtn);
+
+        const linha = document.createElement('hr');
+
+        // div.appendChild(btnAlterar);
+        // div.appendChild(btnRemover);
         li.appendChild(div);
+        ul.appendChild(linha);
         ul.appendChild(li);
+        
+        
     });
 }
 
